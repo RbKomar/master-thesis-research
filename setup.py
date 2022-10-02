a8834423ba5c23e50af968af272f9ca0181b4c15
Robert <RobKomar98@gmail.com>
2022-10-02 15:41:27 +0200

    create cookiecutter project data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description="Project created to conduct research work for master's thesis",
+    author='Robert Komar',
+    license='MIT',
+)
