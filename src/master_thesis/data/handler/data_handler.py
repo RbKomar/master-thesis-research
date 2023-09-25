ab2357fcfda9d60321984d1a3a90e60712ca5f6c
Robert <RobKomar98@gmail.com>
2023-09-25 23:56:21 +0200

    missplaced return type

--- src/master_thesis/data/handler/data_handler.py
+++ src/master_thesis/data/handler/data_handler.py
@@ -6,13 +6,13 @@ from src.master_thesis.data.management.dataset_manager import DatasetManager
 
 class DataHandler:
     def __init__(self, data_path, img_ext, batch_size, image_parser, obscure_images_percent, train_prop, val_prop,
-                 year) -> DatasetManager:
+                 year):
         csv_loader = CSVLoader(data_path, year)
         self.img_path_processor = ImagePathProcessor(csv_loader.load(), img_ext)
         img_paths, img_labels = self.img_path_processor.process_paths(data_path, train_prop, val_prop)
         self.dataset_creator = DataSetCreator(img_paths, img_labels, image_parser, obscure_images_percent, batch_size)
         self.datasets = self.dataset_creator.create_datasets()
 
-    def get_datasets(self):
+    def get_datasets(self) -> DatasetManager:
         dataset_manager = DatasetManager(self.datasets["train"], self.datasets["val"], self.datasets["test"])
         return dataset_manager
